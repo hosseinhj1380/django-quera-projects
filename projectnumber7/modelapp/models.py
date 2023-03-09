4cdd1995a66bf1d0a44a9d117109930a897bc476
hosseinhj1380 <hosseinhj1380@gmail.com>
2023-03-09 23:08:08 +0330

    in this project i worked on models and make models

---
+++ projectnumber7/modelapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class BookManager(models.Manager):
+    def sort_by_rate(self):
+        return self.order_by('price')
+    
+        
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=10)
+    rate = models.IntegerField(default=0)
+    free = models.BooleanField(default=True)
+    objects=BookManager()
+
+    # CONFIG YOUR MODEL MANAGER HERE
+
+    def __str__(self):
+        return self.name
