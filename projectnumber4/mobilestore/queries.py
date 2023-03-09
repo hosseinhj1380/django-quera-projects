9c81b3ee518600231f3618a5816f967973f00664
hosseinhj1380 <hosseinhj1380@gmail.com>
2023-03-09 23:03:21 +0330

    in this project i worked on queries and filter on that

---
+++ projectnumber4/mobilestore/queries.py
@@ -0,0 +1,32 @@
+from .models import Brand, Mobile
+
+
+def list_all_brands():
+    query = Brand.objects.all()
+    return query
+
+
+def list_all_mobiles():
+    query = Mobile.objects.all()
+    return query
+
+
+def price_of_mobile_with_model(model):
+    query = Mobile.objects.get(model=model).price
+    return query
+
+
+def most_expensive_mobile():
+    query = Mobile.objects.latest('price')
+    return query
+
+
+def all_mobiles_with_brand_of(brand_name):
+    query = Mobile.objects.filter(brand__name=brand_name)
+    return query
+
+
+def all_available_mobiles_with_price_in_range(min_price, max_price):
+    query = Mobile.objects.filter(is_available=True,price__gte=min_price,price__lte=max_price).count()
+
+    return query
